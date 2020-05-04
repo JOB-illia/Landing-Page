d0585efba0f4486ad65408ed995ff793a7f718eb
user.email <job.klenchys.i@gmail.com>
2020-05-04 23:12:40 +0200

    Homework 12

--- js/input.js
+++ js/input.js
@@ -1,5 +1,35 @@
-let age = document.getElementById('age');
-function showUser(surname, name) {
-   alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+//let age = document.getElementById('age');
+//function showUser(surname, name) {
+//   alert("Пользователь " + surname + " " + name + ", его возраст " + this.value);
+//}
+//showUser.apply(age, ['Klen', 'Illia']);
+
+class Option {
+   constructor(height = 300, width = 300, bg = 'red', fontSize = 24, textAlign = 'left', color = '#fff') {
+      this.height = height;
+      this.width = width;
+      this.bg = bg;
+      this.fontSize = fontSize;
+      this.textAlign = textAlign;
+      this.color = color;
+   }
+   createDiv() {
+      let createDivBox = document.createElement('div');
+      createDivBox.innerHTML = '<p>Привет Мир! Это было великолептно</p>';
+      document.body.appendChild(createDivBox);
+      let styleDivBox = `
+            height:${this.height}px;
+            width:${this.width}px;
+            background-color:${this.bg};
+            font-size:${this.fontSize}px;
+            text-align:${this.textAlign}; 
+            color: ${this.color};
+     `;
+      createDivBox.style.cssText = styleDivBox;
+   }
 }
-showUser.apply(age, ['Klen', 'Illia']);
+
+const box = new Option(300, 300, "red", 24, "center");
+
+box.createDiv();
+
